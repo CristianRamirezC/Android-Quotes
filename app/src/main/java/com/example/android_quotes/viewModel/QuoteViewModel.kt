0cb4162bd9894aa7a5dab90e06f01c4aa513aa4d
CristianRamirezC <cristiancrc97@hotmail.com>
2022-10-25 16:36:25 -0500

    creaqting viewModel variables

---
+++ app/src/main/java/com/example/android_quotes/viewModel/QuoteViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.android_quotes.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class QuoteViewModel : ViewModel() {
+
+    private var _quote = MutableLiveData<String>()
+    val quote: LiveData<String> = _quote
+
+    private var _author = MutableLiveData<String>()
+    val author: LiveData<String> = _quote
+
+    private var _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    
+}
