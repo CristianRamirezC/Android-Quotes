445f62404c0d53d447f11917fc2d22a3e9a983b3
CristianRamirezC <cristiancrc97@hotmail.com>
2022-10-25 12:01:01 -0500

    creating the ui for the quote and author

--- app/src/main/java/com/example/android_quotes/MainActivity.kt
+++ app/src/main/java/com/example/android_quotes/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.android_quotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.android_quotes.ui.theme.AndroidQuotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +24,9 @@ class MainActivity : ComponentActivity() {
             AndroidQuotesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    QuoteScreen()
                 }
             }
         }
@@ -32,6 +36,37 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Composable
+fun QuoteScreen(
+    modifier: Modifier = Modifier,
+    quote: String = "\"This is a quote\"",
+    author: String = "Author"
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF4F1497)),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier,
+            text = quote,
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            color = Color.White
+        )
+        Text(
+            modifier = Modifier,
+            text = author,
+            textAlign = TextAlign.End,
+            fontSize = 25.sp,
+            color = Color.White
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
