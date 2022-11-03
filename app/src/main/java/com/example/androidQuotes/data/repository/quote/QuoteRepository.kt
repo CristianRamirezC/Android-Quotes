d55d76b4426cb1daf466442e8c03fc69b2ff8ced
CristianRamirezC <cristiancrc97@hotmail.com>
2022-11-03 14:11:31 -0500

    adding dependency injection to quoteRepository

--- app/src/main/java/com/example/androidQuotes/data/repository/quote/QuoteRepository.kt
+++ app/src/main/java/com/example/androidQuotes/data/repository/quote/QuoteRepository.kt
@@ -3,8 +3,9 @@ package com.example.androidQuotes.data.repository.quote
 import com.example.androidQuotes.data.database.quote.QuoteProvider
 import com.example.androidQuotes.data.model.quote.QuoteModel
 import com.example.androidQuotes.data.network.quote.QuoteService
+import javax.inject.Inject
 
-class QuoteRepository {
+class QuoteRepository @Inject constructor() {
 
     private val api = QuoteService()
     suspend fun getAllQuotes(): List<QuoteModel> {
--- app/src/main/java/com/example/androidQuotes/domain/quote/GetQuotesUseCase.kt
+++ app/src/main/java/com/example/androidQuotes/domain/quote/GetQuotesUseCase.kt
@@ -4,8 +4,9 @@ import com.example.androidQuotes.data.model.quote.QuoteModel
 import com.example.androidQuotes.data.repository.quote.QuoteRepository
 import javax.inject.Inject
 
-class GetQuotesUseCase @Inject constructor() {
-    private val repository = QuoteRepository()
+class GetQuotesUseCase @Inject constructor(
+    private val repository: QuoteRepository
+) {
 
     suspend operator fun invoke(): List<QuoteModel> = repository.getAllQuotes()
 }
